8f668887e148a6a9126e4705c460657923e634f8
Owais Eltigani <120919133+Owais-Eltigani@users.noreply.github.com>
2025-06-09 22:04:09 +0530

    Update middleware
    disabled all the public routes

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,8 @@ import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
   publicRoutes: [
-    '/',
     '/api/webhook',
-    '/question/:id',
-    '/tags',
-    '/tags/:id',
-    '/profile/:id',
-    '/community',
-    '/jobs',
+   
   ],
   ignoredRoutes: ['/api/webhook', '/api/chatgpt'],
 });
@@ -17,3 +11,12 @@ export default authMiddleware({
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+ // '/question/:id',
+ //    '/tags',
+ //    '/tags/:id',
+ //    '/profile/:id',
+ //    '/community',
+ //    '/jobs',
+ //    '/',
+   
