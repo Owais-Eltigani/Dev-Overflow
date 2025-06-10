8e54f4bf49460d78182e851bc200cdfb8acccb80
Owais Eltigani <120919133+Owais-Eltigani@users.noreply.github.com>
2025-06-10 14:26:53 +0530

    Update middleware
    made the main page public route

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { authMiddleware } from '@clerk/nextjs';
 export default authMiddleware({
   publicRoutes: [
     '/api/webhook',
+    '/',
    
   ],
   ignoredRoutes: ['/api/webhook', '/api/chatgpt'],
